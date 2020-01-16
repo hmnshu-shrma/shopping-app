ad298ebbb7197c213089582a0a9af91527e08705
hmnshu-shrma <hmnshu2690@gmiail.com>
2020-01-16 16:50:54 +0530

    server json added

--- server.js
+++ server.js
@@ -2,12 +2,31 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-const data = require(__dirname + '/src_besrv/server/addToCart/index.post.json')
 
-app.get('/success', function (req, res) {
+
+app.get('/add-to-cart', function (req, res) {
+  const addtocart = require(__dirname + '/src_besrv/server/addToCart/index.post.json')
   res.header("Content-Type",'application/json');
-  res.status(200) 
-  res.send(JSON.stringify(data));
+  res.status(200)
+  res.send(JSON.stringify(addtocart));
+})
+app.get('/banner-images', function (req, res) {
+  const banner  = require(__dirname + '/src_besrv/server/categories/index.get.json')
+  res.header("Content-Type",'application/json')
+  res.status(200)
+  res.send(JSON.stringify(banner))
+})
+app.get('/categories', function (req, res) {
+  const categories  = require(__dirname + '/src_besrv/server/categories/index.get.json')
+  res.header("Content-Type",'application/json')
+  res.status(200)
+  res.send(JSON.stringify(categories));
+})
+app.get('/products', function (req, res) {
+  const products  = require(__dirname + '/src_besrv/server/products/index.get.json')
+   res.header("Content-Type",'application/json');
+  res.status(200)
+  res.send(JSON.stringify(products));
 })
 
 // console.log that your server is up and running
@@ -15,8 +34,5 @@ app.listen(port, () => console.log(`Listening on port ${port}`))
 
 // create a GET route
 app.get('/api', (req, res) => {
-
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' })
-
-
 })
