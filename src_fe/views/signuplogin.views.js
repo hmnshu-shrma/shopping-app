9a74a4742b68db151e77588776c7dac44b77a465
Himanshu Sharma <himsharm9@publicisgroupe.net>
2020-01-21 11:31:12 +0530

    message

---
+++ src_fe/views/signuplogin.views.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginSignup = (props) => {
+  return (
+    <React.Fragment>
+      <div className='login-contaner'>
+        <p className='link signup'>Signup </p> | 
+        <p className='link login'>login</p>
+      </div>
+    </React.Fragment>
+  )
+}
+export default LoginSignup
