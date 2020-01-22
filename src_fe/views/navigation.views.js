e677a45af696c1c9746691f1ec23f645bd528948
Himanshu Sharma <himsharm9@publicisgroupe.net>
2020-01-22 09:52:54 +0530

    new changes

--- src_fe/views/navigation.views.js
+++ src_fe/views/navigation.views.js
@@ -1,32 +1,35 @@
 import React from 'react'
-
 import Cart from './cart.views'
+import LoginSignup from './signuplogin.views'
 import Logo from 'Assets/images/logo.png'
 import '../styles/navigation.scss'
 class Navigation extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <nav>
-          <div className='navigation'>
-            <div className='logo-container'>
-              <img className='logo-img' src={Logo} alt='logo image' />
+        <header>
+          <nav>
+            <div className='navigation'>
+              <div className='logo-container'>
+                <img className='logo-img' src={Logo} alt='logo image' />
+              </div>
+              <div className='navlinks-container'>
+                <ul className='links'>
+                  <li>
+                    Home
+                  </li>
+                  <li>
+                    Products
+                  </li>
+                </ul>
+              </div>
+              <div className='cart-login-container'>
+                <Cart />
+                <LoginSignup />
+              </div>
             </div>
-            <div className='navlinks-container'>
-              <ul className='links'>
-                <li>
-                Home
-                </li>
-                <li>
-                Products
-                </li>
-              </ul>
-            </div>
-            <div className='cart-login-container'>
-              <Cart />
-            </div>
-          </div>
-        </nav>
+          </nav>
+        </header>
       </React.Fragment>
     )
   }
--- src_fe/views/signuplogin.views.js
+++ src_fe/views/signuplogin.views.js
@@ -4,7 +4,7 @@ const LoginSignup = (props) => {
   return (
     <React.Fragment>
       <div className='login-contaner'>
-        <p className='link signup'>Signup </p> | 
+        <p className='link signup'>Signup </p>
         <p className='link login'>login</p>
       </div>
     </React.Fragment>
